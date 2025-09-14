b513a220de4409d1efe7da386b1e6a32de8be345
xPshycho <edwinbalbuena189@gmail.com>
2025-09-14 14:29:05 -0400

    add chat, message, user models and message type enum

---
+++ app/src/main/java/com/proyecto/chatapp/data/models/MessageType.java
@@ -0,0 +1,6 @@
+package com.proyecto.chatapp.data.models;
+
+public enum MessageType {
+    TEXT,
+    IMAGE
+}
---
+++ app/src/main/java/com/proyecto/chatapp/data/models/User.java
@@ -0,0 +1,93 @@
+package com.proyecto.chatapp.data.models;
+
+import com.google.firebase.firestore.PropertyName;
+
+public class User {
+    private String id;
+    private String email;
+    private String name;
+    private String profileImageUrl;
+    private boolean isOnline;
+    private long lastSeen;
+
+    public User() {
+    }
+
+    public User(String id, String email, String name) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+        this.profileImageUrl = "";
+        this.isOnline = false;
+        this.lastSeen = System.currentTimeMillis();
+    }
+
+    public User(String id, String email, String name, String profileImageUrl, boolean isOnline, long lastSeen) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+        this.profileImageUrl = profileImageUrl;
+        this.isOnline = isOnline;
+        this.lastSeen = lastSeen;
+    }
+
+    @PropertyName("id")
+    public String getId() {
+        return id;
+    }
+
+    @PropertyName("id")
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @PropertyName("email")
+    public String getEmail() {
+        return email;
+    }
+
+    @PropertyName("email")
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @PropertyName("name")
+    public String getName() {
+        return name;
+    }
+
+    @PropertyName("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @PropertyName("profileImageUrl")
+    public String getProfileImageUrl() {
+        return profileImageUrl;
+    }
+
+    @PropertyName("profileImageUrl")
+    public void setProfileImageUrl(String profileImageUrl) {
+        this.profileImageUrl = profileImageUrl;
+    }
+
+    @PropertyName("isOnline")
+    public boolean isOnline() {
+        return isOnline;
+    }
+
+    @PropertyName("isOnline")
+    public void setOnline(boolean online) {
+        isOnline = online;
+    }
+
+    @PropertyName("lastSeen")
+    public long getLastSeen() {
+        return lastSeen;
+    }
+
+    @PropertyName("lastSeen")
+    public void setLastSeen(long lastSeen) {
+        this.lastSeen = lastSeen;
+    }
+}
