35ffebb93d471e3426db73ff8e995bec07645571
Arturo <axab0001@ce.pucmm.edu.do>
2025-09-16 16:26:54 -0400

    Creacion de la pantalla de carga

--- Klk/app/build.gradle.kts
+++ Klk/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.klk"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.klk"
@@ -32,7 +32,7 @@ dependencies {
 
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.13.0")
-    implementation("androidx.activity:activity:1.11.0")
+//    implementation("androidx.activity:activity:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
