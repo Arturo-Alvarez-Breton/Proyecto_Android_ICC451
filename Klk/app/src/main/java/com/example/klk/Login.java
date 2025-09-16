88ac1b7d32d604ff1bb91c244feeeb9336dd9329
Arturo <axab0001@ce.pucmm.edu.do>
2025-09-16 17:06:39 -0400

    Login inicial

--- Klk/app/src/main/java/com/example/klk/Login.java
+++ Klk/app/src/main/java/com/example/klk/Login.java
@@ -1,32 +1,38 @@
 package com.example.klk;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SplashActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_splash);
-
-        new Handler().postDelayed(() -> {
-            startActivity(new Intent(SplashActivity.this, Login.class));
-            finish();
-        }, 1000);
-
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button btnRegister = findViewById(R.id.btnRegister);
+        Button btnLogin = findViewById(R.id.btnLogin);
+        View emailField = findViewById(R.id.email);
+
+        btnRegister.setOnClickListener( v ->{
+            emailField.setVisibility(View.VISIBLE);
+        });
+
+        btnLogin.setOnClickListener( v ->{
+            emailField.setVisibility(View.GONE);
         });
     }
 }
